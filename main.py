648edd70eb7bba88a15be017652f07cabcbacdd7
hoangperry <prodai9573@gmail.com>
2020-04-06 13:50:12 +0700

    update test

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from selenium_driver import WebDriver
+from selenium.webdriver.chrome.options import Options
+import os
+import yaml
 
 
 if __name__ == '__main__':
     options = Options()
     options.add_argument('--dns-prefetch-disable')
     options.headless = False
-    form = WebDriver(start_url, executable_path=os.getcwd() + '/chromedriver', options=options)
+    ques = yaml.safe_load(open('question_meta.yaml', encoding='utf-8'))
+    form = WebDriver(ques, executable_path=os.getcwd() + '/chromedriver', options=options)
+    form.run(100)
